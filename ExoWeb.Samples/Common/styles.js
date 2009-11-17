1820283a0339c85f5e4a453896f202c74dc247dc
Matt <matt.hooper@vc3.com>
2009-11-17 13:30:15 -0500

    ...

---
+++ ExoWeb.Samples/Common/styles.js
@@ -0,0 +1,18 @@
+﻿function JsStyleSheet(el) {
+
+	$("input", el).validated(function(sender, issues) {
+		if (issues.length == 0) {
+			$(this).removeClass("error");
+			$(this).attr("title", null);
+		} else {
+			$(this).addClass("error");
+			$(this).attr("title", issues[0].get_message());
+		}
+	});
+}
+
+JsStyleSheet();
+
+$("*").rendered(function() {
+	JsStyleSheet(this);
+});
