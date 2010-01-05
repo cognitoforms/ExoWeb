cb65d4ef4ee0f4a4f2d215ea4fd5e20993e851ff
Peter <peter.gindhart@vc3.com>
2010-01-05 11:46:22 -0500

    begin placement options implementation; enrich radio/checkbox list templates

--- ExoWeb/Client/exoweb.ui.js
+++ ExoWeb/Client/exoweb.ui.js
@@ -388,4 +388,25 @@ Type.registerNamespace("ExoWeb.UI");
 		execute();
 	}
 
+	//////////////////////////////////////////////////////////////////////////////////////
+	// MS AJAX overrides
+
+	/// replaced implementation to use _tcindex instead of _index
+	/// http://msmvps.com/blogs/luisabreu/archive/2009/10/19/the-dataview-control-going-imperative-take-iii.aspx
+	Sys.UI.TemplateContext.prototype.getInstanceId = function(prefix) {
+		var s;
+		if (this._global) {
+			s = "";
+		}
+		else {
+			s = this._tcindex;
+			var ctx = this.parentContext;
+			while (ctx && !ctx._global) {
+				s = ctx._tcindex + "_" + s;
+				ctx = ctx.parentContext;
+			}
+		}
+		return prefix + s;
+	}
+
 })();
