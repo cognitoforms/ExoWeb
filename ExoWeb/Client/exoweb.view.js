e785049d875e919da0f2ed76f7dd2bdeb25d646a
Bryan <mattheyan@gmail.com>
2010-01-11 11:03:19 -0500

    account for radio button binding bug

--- ExoWeb/Client/exoweb.view.js
+++ ExoWeb/Client/exoweb.view.js
@@ -511,10 +511,38 @@
 		}
 		ExoWeb.View.OptionAdapter = OptionAdapter;
 		OptionAdapter.registerClass("ExoWeb.View.OptionAdapter");
+		
+		(function() {
+			var impl = Sys.Binding.prototype._targetChanged;
+			Sys.Binding.prototype._targetChanged = function Sys$Binding$_targetChangedOverride(force){
+
+				// invoke the method implementation
+				impl.apply(this, [force]);
+
+				var target = this._target;
+
+				// Set _lastTarget=false on other radio buttons in the group, since they only 
+				// remember the last target that was recieved when an event fires and radio button
+				// target change events fire on click (which does not account for de-selection).  
+				// Otherwise, the source value is only set the first time the radio button is selected.
+				if ($(target).is("input[type=radio]")) {
+					$("input[type=radio][name='" + target.name + "']").each(
+						function updateRadioLastTarget() {
+							if (this != target) {
+								var bindings = this.__msajaxbindings;
+								for (var i = 0; i < bindings.length; i++) {
+									bindings[i]._lastTarget = false;
+								}
+							}
+						}
+					);
+				}
+			}
+		})();
 	}
 
 	if (Sys && Sys.loader) {
-		Sys.loader.registerScript("ExoWebJquery", null, execute);
+		Sys.loader.registerScript("ExoWebView", null, execute);
 	}
 	else {
 		execute();
