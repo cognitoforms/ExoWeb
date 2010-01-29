8fe5d8c6c41cc448e6cbe35143a0651b86b29bf8
Bryan <mattheyan@gmail.com>
2010-01-29 17:34:21 -0500

    move template context method override within execute method since scripts are fetched aggressively (this was causing errors in other browsers)

--- ExoWeb/Client/exoweb.ui.js
+++ ExoWeb/Client/exoweb.ui.js
@@ -519,6 +519,26 @@ Type.registerNamespace("ExoWeb.UI");
 
 		window.$isLast = getIsLast;
 
+		//////////////////////////////////////////////////////////////////////////////////////
+		// MS AJAX overrides
+
+		/// replaced implementation to use _tcindex instead of _index
+		/// http://msmvps.com/blogs/luisabreu/archive/2009/10/19/the-dataview-control-going-imperative-take-iii.aspx
+		Sys.UI.TemplateContext.prototype.getInstanceId = function(prefix) {
+			var s;
+			if (this._global) {
+				s = "";
+			}
+			else {
+				s = this._tcindex;
+				var ctx = this.parentContext;
+				while (ctx && !ctx._global) {
+					s = ctx._tcindex + "_" + s;
+					ctx = ctx.parentContext;
+				}
+			}
+			return prefix + s;
+		}
 	}
 
 	if (window.Sys && Sys.loader) {
@@ -526,27 +546,6 @@ Type.registerNamespace("ExoWeb.UI");
 	}
 	else {
 		execute();
-	}
-
-	//////////////////////////////////////////////////////////////////////////////////////
-	// MS AJAX overrides
-
-	/// replaced implementation to use _tcindex instead of _index
-	/// http://msmvps.com/blogs/luisabreu/archive/2009/10/19/the-dataview-control-going-imperative-take-iii.aspx
-	Sys.UI.TemplateContext.prototype.getInstanceId = function(prefix) {
-		var s;
-		if (this._global) {
-			s = "";
-		}
-		else {
-			s = this._tcindex;
-			var ctx = this.parentContext;
-			while (ctx && !ctx._global) {
-				s = ctx._tcindex + "_" + s;
-				ctx = ctx.parentContext;
-			}
-		}
-		return prefix + s;
 	}
 
 })();
