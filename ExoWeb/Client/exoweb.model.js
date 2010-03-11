437c6262b16d592e8fe8bcd64441649435ff0d64
Matt <matt.hooper@vc3.com>
2010-03-11 14:24:32 -0500

    change string conversions for null and undefined to prevent changes from occuring for existing objects when choosing "" values in the ui. update templates to always have a value for <option>'s, otherwise validation errors would result.

--- ExoWeb/Client/exoweb.model.js
+++ ExoWeb/Client/exoweb.model.js
@@ -237,7 +237,7 @@
 		Entity.formats = {
 			$system: new Format({
 				undefinedString: "",
-				nullString: "null",
+				nullString: "",
 				convert: function(obj) {
 					return $format("{0}|{1}", [obj.meta.type.get_fullName(), obj.meta.id]);
 				},
