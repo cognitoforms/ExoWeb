214fe181ba99b0f64057c5ed1f9397dd8516c080
Bryan <mattheyan@gmail.com>
2010-04-28 15:14:45 -0400

    only run radio button binding logic if the target of the binding is an html element

--- ExoWeb/Client/exoweb.view.js
+++ ExoWeb/Client/exoweb.view.js
@@ -701,23 +701,25 @@
 				// invoke the method implementation
 				impl.apply(this, [force]);
 
-				var target = this._target;
+				if (this._target instanceof Element) {
+					var target = this._target;
 
-				// Set _lastTarget=false on other radio buttons in the group, since they only 
-				// remember the last target that was recieved when an event fires and radio button
-				// target change events fire on click (which does not account for de-selection).  
-				// Otherwise, the source value is only set the first time the radio button is selected.
-				if ($(target).is("input[type=radio]")) {
-					$("input[type=radio][name='" + target.name + "']").each(
-						function updateRadioLastTarget() {
-							if (this != target) {
-								var bindings = this.__msajaxbindings;
-								for (var i = 0; i < bindings.length; i++) {
-									bindings[i]._lastTarget = false;
+					// Set _lastTarget=false on other radio buttons in the group, since they only 
+					// remember the last target that was recieved when an event fires and radio button
+					// target change events fire on click (which does not account for de-selection).  
+					// Otherwise, the source value is only set the first time the radio button is selected.
+					if ($(target).is("input[type=radio]")) {
+						$("input[type=radio][name='" + target.name + "']").each(
+							function updateRadioLastTarget() {
+								if (this != target) {
+									var bindings = this.__msajaxbindings;
+									for (var i = 0; i < bindings.length; i++) {
+										bindings[i]._lastTarget = false;
+									}
 								}
 							}
-						}
-					);
+						);
+					}
 				}
 			};
 		})();
