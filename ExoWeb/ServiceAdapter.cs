2231a296f33fe1a93e9542104dad2acf1e3d87fd
Bryan <mattheyan@gmail.com>
2010-05-15 16:41:32 -0400

    GetConditions thread safety.

--- ExoWeb/ServiceAdapter.cs
+++ ExoWeb/ServiceAdapter.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using ExoGraph;
 using ExoRule;
 
@@ -27,6 +24,4 @@ namespace ExoWeb
 	}
 
 	#endregion
-
-
 }
