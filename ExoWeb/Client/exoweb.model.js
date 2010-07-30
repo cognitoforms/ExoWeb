7b00347984db6075e30647c95d07b336e2f4a788
Bryan <mattheyan@gmail.com>
2010-07-30 09:00:11 -0400

    Default flag field to false

--- ExoWeb/Client/exoweb.model.js
+++ ExoWeb/Client/exoweb.model.js
@@ -863,6 +863,9 @@
 		}
 
 		Property.mixin({
+			defaultValue: function Property$defaultValue(value) {
+				this._containingType._initNewProps.push({ property: this, valueFn: function() { return value; } });
+			},
 			equals: function Property$equals(prop) {
 				if (prop !== undefined && prop !== null) {
 					if (prop instanceof Property) {
