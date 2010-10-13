a71843c1d08174451b46e6dbf79a423f0a8a993a
Bobby <kr4ster@gmail.com>
2010-10-13 08:58:31 -0400

    Removed reference to System.Web.Mvc

--- ExoWeb/ExoWeb.cs
+++ ExoWeb/ExoWeb.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ExoGraph;
-using System.Web.Mvc;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Web.Script.Serialization;
