a50ae34f6b2a832036c5cb4f26ef9a027b7361bb
Bryan <mattheyan@gmail.com>
2011-04-07 10:08:53 -0400

    Incorporate cache hash in type definition cacheing.

--- ExoWeb/Client/dist/exoweb-base.js
+++ ExoWeb/Client/dist/exoweb-base.js
@@ -6572,22 +6572,34 @@ Type.registerNamespace("ExoWeb.Mapper");
 		}
 
 		var batch = ExoWeb.Batch.suspendCurrent("typeProvider");
+
 		var cachedType = ExoWeb.cache(type);
 		if (cachedType) {
-			onSuccess.call(thisPtr || this, cachedType);
+			if (ExoWeb.cacheHash && cachedType.cacheHash !== ExoWeb.cacheHash) {
+				// the cached type definition is out of date, so remove it and continue
+				ExoWeb.cache(type, null);
+			}
+			else {
+				// the cached type definition is current, so use it and return early
+				onSuccess.call(thisPtr || this, cachedType);
+				return;
+			}
 		}
-		else {
-			typeProviderFn.call(this, type,
-				function typeProviderSuccess() {
-					ExoWeb.Batch.resume(batch);
-					ExoWeb.cache(type, arguments[0]);
-					if (onSuccess) onSuccess.apply(thisPtr || this, arguments);
-				},
-				function typeProviderFailure() {
-					ExoWeb.Batch.resume(batch);
-					if (onFailure) onFailure.apply(thisPtr || this, arguments);
-				});
-		}
+
+		typeProviderFn.call(this, type,
+			function typeProviderSuccess() {
+				ExoWeb.Batch.resume(batch);
+
+				// add cache hash and cache type definition
+				arguments[0].cacheHash = ExoWeb.cacheHash;
+				ExoWeb.cache(type, arguments[0]);
+
+				if (onSuccess) onSuccess.apply(thisPtr || this, arguments);
+			},
+			function typeProviderFailure() {
+				ExoWeb.Batch.resume(batch);
+				if (onFailure) onFailure.apply(thisPtr || this, arguments);
+			});
 	}
 	ExoWeb.Mapper.setTypeProvider = function setTypeProvider(fn) {
 		typeProviderFn = fn;
