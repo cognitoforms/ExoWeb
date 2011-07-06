4076a5684cb198867b9a2ee1e1bf8abf547972e0
Brandon <brandon.poe@vc3.com>
2011-07-06 12:03:47 -0400

    Resolved issue where the selected item for a radio button was changed through the model, then set in the UI, and then changed by the model again but the radio button does not show as selected.

--- ExoWeb/Client/src/base/view/MsAjax.js
+++ ExoWeb/Client/src/base/view/MsAjax.js
@@ -1,23 +1,36 @@
-(function() {
-	var impl = Sys.Binding.prototype._targetChanged;
-	Sys.Binding.prototype._targetChanged = function(force) {
+(function () {
+	var targetChangedImpl = Sys.Binding.prototype._targetChanged;
+	Sys.Binding.prototype._targetChanged = function (force) {
 		var target = this._target;
 
 		// invoke the method implementation
-		impl.apply(this, [force]);
+		targetChangedImpl.apply(this, [force]);
 
 		// Set _lastTarget=false on other radio buttons in the group, since they only 
 		// remember the last target that was recieved when an event fires and radio button
 		// target change events fire on click (which does not account for de-selection).  
 		// Otherwise, the source value is only set the first time the radio button is selected.
 		if (Sys.UI.DomElement.isDomElement(target) && $(target).is("input[type=radio]")) {
-			$("input[type=radio][name='" + target.name + "']").each(function() {
+			$("input[type=radio][name='" + target.name + "']").each(function () {
 				if (this != target && this.__msajaxbindings !== undefined) {
 					var bindings = this.__msajaxbindings;
 					for (var i = 0; i < bindings.length; i++)
-						bindings[i]._lastTarget = false;
+						bindings[i]._lastTarget = bindings[i]._lastSource = false;
 				}
 			});
+		}
+	};
+
+	var sourceChangedImpl = Sys.Binding.prototype._sourceChanged;
+	Sys.Binding.prototype._sourceChanged = function (force) {
+		var target = this._target;
+
+		// invoke the method implementation
+		sourceChangedImpl.apply(this, [force]);
+
+		// Remove checked attribute from other radio buttons in the group that are currently checked.
+		if (Sys.UI.DomElement.isDomElement(target) && $(target).is("input[type=radio]") && !this._lastSource) {
+			$(target).removeAttr("checked");
 		}
 	};
 })();
