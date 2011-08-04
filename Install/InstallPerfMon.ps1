02033697ec5c1ee871519a87cb8eca5d9ed9fafd
Matt <matt.hooper@vc3.com>
2011-08-04 14:04:57 -0400

    clarify counter descriptions

--- Install/InstallPerfMon.ps1
+++ Install/InstallPerfMon.ps1
@@ -14,27 +14,27 @@ $counters = New-Object System.Diagnostics.CounterCreationDataCollection
 
 $ignore = $counters.Add( (New-Object $ccdType `
 	"Requests",
-	"Total number of times the ExoWeb has been invoked for data queries, round trips and server events. Includes both local and remote queries.  Type load requests and error log requests are not included in this number.",
+	"Total number of times the ExoWeb has been invoked for data queries, round trips, server events and types. Includes both local and remote queries.",
 	NumberOfItems32) );
 
 $ignore = $counters.Add( (New-Object $ccdType `
 	"Local Requests",
-	"Total number locally-requested ExoWeb queries. Excludes queries sent by clients to the ExoWeb web service.",
+	"Total number  of locally-requested ExoWeb queries. Excludes queries sent by clients to the ExoWeb web service.",
 	NumberOfItems32) );
 
 $ignore = $counters.Add( (New-Object $ccdType `
 	"Remote Requests",
-	"Total number of times the ExoWeb web service has been invoked for data queries, round trips and server events. Type load requests and error log requests are not included in this number.",
+	"Total number of times the ExoWeb web service has been invoked for data queries, round trips and server events. Type load requests are also included in this number.",
 	NumberOfItems32) );
 
 $ignore = $counters.Add( (New-Object $ccdType `
 	"Remote Request Bytes In",
-	"Total number of bytes sent to the ExoWeb web service for data queries, round trips and server events.  Type load requests and error log requests are not included in this number.",
+	"Total number of bytes sent to the ExoWeb web service for data queries, round trips and server events.",
 	NumberOfItems32) );
 
 $ignore = $counters.Add( (New-Object $ccdType `
 	"Remote Request Change Log Actions In",
-	"Total change log entries sent to the ExoWeb web service during data queries, round trips and server events.  Type load requests and error log requests are not included in this number.",
+	"Total change log entries sent to the ExoWeb web service during data queries, round trips and server events.",
 	NumberOfItems32) );
 
 $counters | foreach { 
