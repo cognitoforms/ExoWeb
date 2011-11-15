eaf93c990383abdbc45d123866f802a241898bbd
Bryan <mattheyan@gmail.com>
2011-11-15 09:38:51 -0500

    Rename unit test project.

--- ExoWeb.UnitTests.QUnit/Properties/AssemblyInfo.cs
+++ ExoWeb.UnitTests.QUnit/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExoWeb.UnitTests")]
+[assembly: AssemblyTitle("ExoWeb.UnitTests.QUnit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExoWeb.UnitTests")]
+[assembly: AssemblyProduct("ExoWeb.UnitTests.QUnit")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
