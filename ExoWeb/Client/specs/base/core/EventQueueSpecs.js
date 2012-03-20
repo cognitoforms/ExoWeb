e32250572cc5c18828ea342e39e3a25e26655815
Bryan <mattheyan@gmail.com>
2012-03-20 12:08:57 -0400

    simple event queue tests

--- ExoWeb/Client/specs/base/core/EventQueueSpecs.js
+++ ExoWeb/Client/specs/base/core/EventQueueSpecs.js
@@ -13,11 +13,58 @@ jasmine.jasmine.debug = true;
 var describe = jasmine.describe;
 var it = jasmine.it;
 var expect = jasmine.expect;
+var beforeEach = jasmine.beforeEach;
 
 // Test Suites
 ///////////////////////////////////////
 
 describe("EventQueue", function() {
+	it("raises events immediately if queueing has not been enabled", function() {
+		var value = 0;
+		var queue = new ExoWeb.EventQueue(function(amount) {
+			value += amount;
+		});
+
+		queue.push(5);
+		expect(value).toBe(5);
+	});
+
+	it("defers raising events until queueing is stopped", function() {
+		var value = 0;
+		var queue = new ExoWeb.EventQueue(function(amount) {
+			value += amount;
+		});
+
+		queue.startQueueing();
+		queue.push(5);
+		queue.push(-1);
+		expect(value).toBe(0);
+		queue.stopQueueing();
+		expect(value).toBe(4);
+	});
+
+	it("only raises distinct events according to the equality function", function() {
+		var value = 0;
+		var queue = new ExoWeb.EventQueue(
+			function(amount) {
+				value += amount;
+			},
+			function(amount) {
+				return amount === amount;
+			}
+		);
+
+		queue.push(5);
+		queue.push(5);
+		expect(value).toBe(10);
+
+		value = 0;
+		queue.startQueueing();
+		queue.push(5);
+		queue.push(5);
+		queue.stopQueueing();
+		expect(value).toBe(5);
+	});
 });
 
 // Run Tests
