93db75fb21a247f04dcafe2d3f79c16f811431fd
Mark <mark.philipp@vc3.com>
2012-04-24 17:11:33 -0400

    Adding missing init line for variable in checkAllowedValuesExist

--- ExoWeb/Client/src/base/view/Adapter.js
+++ ExoWeb/Client/src/base/view/Adapter.js
@@ -606,6 +606,7 @@ function checkAllowedValuesRuleExists(rule) {
 function checkAllowedValuesExist() {
 	var lastProperty = this._propertyChain.lastProperty();
 	var allowedValuesRule = lastProperty.rule(ExoWeb.Model.Rule.allowedValues);
+	var targetObj = this._propertyChain.lastTarget(this._target);
 	var allowedValues = allowedValuesRule.values(targetObj, !!this._allowedValuesMayBeNull);
 
 	if (allowedValues instanceof Array) {
