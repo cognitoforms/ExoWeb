e341204c4b45a1aa74867772909d7d1d3374a958
Jonathan Parker <jonathan.parker@vc3.com>
2012-06-12 15:22:36 -0400

    Added pendingInit function to Type.js to support rules on static properties
    Signed-off-by: Bryan <mattheyan@gmail.com>

--- ExoWeb/Client/src/base/model/Type.js
+++ ExoWeb/Client/src/base/model/Type.js
@@ -12,6 +12,7 @@ function Type(model, name, baseType, origin) {
 	this._properties = {}; 
 	this._instanceProperties = {};
 	this._staticProperties = {};
+	this._pendingInit = [];
 
 	// define properties
 	Object.defineProperty(this, "model", { value: model });
@@ -164,6 +165,20 @@ Type.prototype = {
 	addInitNew: function Type$addInitNew(handler, obj, once) {
 		this._addEvent("initNew", handler, obj ? equals(obj) : null, once);
 		return this;
+	},
+
+	// gets and optionally sets the pending initialization status for a property on the current instance
+	pendingInit: function Type$pendingInit(prop, value) {
+		var result = this[prop._fieldName] === undefined || this._pendingInit[prop.get_name()] === true;
+		if (arguments.length > 1) {
+			if (value) {
+				this._pendingInit[prop.get_name()] = true;
+			}
+			else {
+				delete this._pendingInit[prop.get_name()];
+			}
+		}
+		return result;
 	},
 	addInitExisting: function Type$addInitExisting(handler, obj, once) {
 		this._addEvent("initExisting", handler, obj ? equals(obj) : null, once);
