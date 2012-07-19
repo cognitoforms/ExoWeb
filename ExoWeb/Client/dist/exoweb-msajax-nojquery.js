c1f95120358847b305cc5f4baa04882f4f34374f
Bryan <mattheyan@gmail.com>
2012-07-18 20:41:52 -0400

    Temporarily rollback calculated property change filtering and avoid introducing duplicate items in lists.

--- ExoWeb/Client/dist/exoweb-msajax-nojquery.js
+++ ExoWeb/Client/dist/exoweb-msajax-nojquery.js
@@ -9641,14 +9641,6 @@ window.ExoWeb.DotNet = {};
 		canSend: function (change) {
 			if (change.type === "Checkpoint") return false;
 
-			// Don't send changes to calculated properties since they can be reproduced at any point when accessed.
-			if (change.type === "ListChange" || change.type === "ReferenceChange" || change.type === "ValueChange") {
-				var jstype = ExoWeb.Model.Model.getJsType(change.instance.type, true);
-				if (jstype && LazyLoader.isLoaded(jstype.meta) && jstype.meta.property(change.property).get_isCalculated()) {
-					return false;
-				}
-			}
-
 			return true;
 		},
 		canSaveObject: function ServerSync$canSaveObject(objOrMeta) {
