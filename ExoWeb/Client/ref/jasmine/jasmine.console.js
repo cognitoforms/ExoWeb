526647c40d8f60bcb1ad22820e3239631cdbd8f1
Bryan <mattheyan@gmail.com>
2013-07-08 13:01:42 -0400

    Use "quiet" logging by default when specs are run interactively to reduce noise and highlight errors when they occur.

--- ExoWeb/Client/ref/jasmine/jasmine.console.js
+++ ExoWeb/Client/ref/jasmine/jasmine.console.js
@@ -1,6 +1,20 @@
 function Reporter() {
 	this.suites = [];
-	this.suppressDateAndTime = process.argv.indexOf("-no-date-time") >= 0;
+	this.suppressDateAndTime = false;
+	this.verbosity = "quiet";
+
+	var self = this;
+	var verbosityExpr = /\-\-v(erbosity)?\:(.*)/;
+	process.argv.forEach(function (arg) {
+		if (arg === "--no-date-time" || arg === "-no-date-time") {
+			self.suppressDateAndTime = true;
+		} else {
+			var verbosityMatch = verbosityExpr.exec(arg);
+			if (verbosityMatch) {
+				self.verbosity = verbosityMatch[2] || "quiet";
+			}
+		}
+	});
 };
 
 Reporter.prototype.isFirstEncounter = function(suite) {
@@ -14,7 +28,6 @@ Reporter.prototype.isFirstEncounter = function(suite) {
 };
 
 Reporter.prototype.reportRunnerStarting = function(runner) {
-	var showPassed, showSkipped;
 	this.startedAt = new Date();
 };
 
@@ -23,13 +36,15 @@ Reporter.prototype.reportRunnerResults = function(runner) {
 	var specs = runner.specs();
 	var message = "\r\n" + specs.length + " spec" + (specs.length === 1 ? "" : "s" ) + ", " + results.failedCount + " failure" + ((results.failedCount === 1) ? "" : "s");
 
-	if (this.suppressDateAndTime !== true)
+	if (this.suppressDateAndTime !== true) {
 		message += " in " + ((new Date().getTime() - this.startedAt.getTime()) / 1000) + "s";
+	}
 
 	console.info(message + "\r\n");
-	
-	if (this.suppressDateAndTime !== true)
+
+	if (this.suppressDateAndTime !== true) {
 		console.info("Finished at " + new Date().toString() + "\r\n");
+	}
 
 	if (results.failedCount > 0) {
 		process.exit(1);
@@ -41,33 +56,34 @@ Reporter.prototype.reportSuiteResults = function(suite) {
 	var status = results.totalCount > 0 ?
 		(results.passed() ? 'passed' : 'failed') : "skipped or empty";
 
-	if (status === "skipped or empty" && this.isFirstEncounter(suite)) {
-		console.log("\r\n" + suite.description);
-		console.log("=============================================");
-	}
-	else if (!suite.parentSuite) {
-		console.log("---------------------------------------------");
-	}
+	if (this.verbosity === "full" || status !== "passed") {
+		if (status === "skipped or empty" && this.isFirstEncounter(suite)) {
+			console.log("\r\n" + suite.description);
+			console.log("=============================================");
+		} else if (!suite.parentSuite) {
+			console.log("---------------------------------------------");
+		}
 
-	if (!suite.parentSuite) {
-		console.log("suite " + status);
-		console.log("=============================================\r\n");
+		if (!suite.parentSuite) {
+			console.log("suite " + status);
+			console.log("=============================================\r\n");
+		}
 	}
 };
 
-Reporter.prototype.reportSpecStarting = function(spec) {
-	if (this.isFirstEncounter(spec.suite) && (!spec.suite.parentSuite || this.isFirstEncounter(spec.suite.parentSuite))) {
-		console.log("\r\nSuite: \"" + (spec.suite.parentSuite ? spec.suite.parentSuite.description : spec.suite.description) + "\"");
-		console.log("=============================================");
-	}
-	else {
-		console.log("---------------------------------------------");
-	}
+Reporter.prototype.printSuiteStart = function (suite) {
+	console.log("\r\nSuite: \"" + (suite.parentSuite ? suite.parentSuite.description : suite.description) + "\"");
+	console.log("=============================================");
+};
 
-	if (spec.suite.parentSuite) {
-		this.suites.push(spec.suite.parentSuite);
+Reporter.prototype.printSpecStart = function (spec) {
+	if (this.verbosity === "full") {
+		if (this.isFirstEncounter(spec.suite) && (!spec.suite.parentSuite || this.isFirstEncounter(spec.suite.parentSuite))) {
+			this.printSuiteStart(spec.suite);
+		} else {
+			console.log("---------------------------------------------");
+		}
 	}
-	this.suites.push(spec.suite);
 
 	var suiteDescription = spec.suite.description;
 	var parentSuite = spec.suite.parentSuite;
@@ -75,16 +91,23 @@ Reporter.prototype.reportSpecStarting = function(spec) {
 		suiteDescription = parentSuite.description + " " + suiteDescription;
 		parentSuite = parentSuite.parentSuite;
 	}
-	console.log("Running \"" + suiteDescription + " " + spec.description + "\"...");
+
+	if (this.verbosity === "full") {
+		console.log("Running \"" + suiteDescription + " " + spec.description + "\"...");
+	}
+};
+
+Reporter.prototype.reportSpecStarting = function(spec) {
+	this.printSpecStart(spec);
+
+	if (spec.suite.parentSuite) {
+		this.suites.push(spec.suite.parentSuite);
+	}
+	this.suites.push(spec.suite);
 };
 
 Reporter.prototype.reportSpecResults = function(spec) {
 	var results = spec.results();
-	var status = results.passed() ? 'passed' : 'failed';
-	if (results.skipped) {
-		status = 'skipped';
-	}
-
 	var resultItems = results.getItems();
 	for (var i = 0; i < resultItems.length; i++) {
 		var result = resultItems[i];
